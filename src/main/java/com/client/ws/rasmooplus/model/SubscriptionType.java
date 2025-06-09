ed6d0951175ccdff884b736ffa7a329cb11f5c3a
Gabriel Vitor da Silva <gvitordasilva@gmail.com>
2025-06-09 15:54:02 -0300

    Criado primeira migration e as entidades do projeto

---
+++ src/main/java/com/client/ws/rasmooplus/model/SubscriptionType.java
@@ -0,0 +1,32 @@
+package com.client.ws.rasmooplus.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "subscriptions_type")
+public class SubscriptionType implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "subscriptions_type_id")
+    private Long id;
+
+    private String name;
+
+    @Column(name = "access_months")
+    private Long accessMonth;
+
+    private BigDecimal price;
+
+    @Column(name = "product_key")
+    private String productKey;
+
+}
---
+++ src/main/java/com/client/ws/rasmooplus/model/UserType.java
@@ -0,0 +1,25 @@
+package com.client.ws.rasmooplus.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "user_type")
+public class UserType implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_type_id")
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+}
